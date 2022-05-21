cb0685b8523ebd2cc79b5f454ed1ba4014662d16
Prabh Chaudhary <prabh2012@hotmail.com>
2022-05-20 20:28:38 -0400

    Day 22 - Drawing App - Third commit - JavaScript completed

--- 22_Drawing_App/script.js
+++ 22_Drawing_App/script.js
@@ -40,3 +40,49 @@ canvas.addEventListener('mousemove', (e) => {
     y = y2
   }
 })
+
+function drawCircle(x, y) {
+  ctx.beginPath()
+  ctx.arc(x, y, size, 0, Math.PI * 2)
+  ctx.fillStyle = color
+  ctx.fill()
+}
+
+function drawLine(x1, y1, x2, y2) {
+  ctx.beginPath()
+  ctx.moveTo(x1, y1)
+  ctx.lineTo(x2, y2)
+  ctx.strokeStyle = color
+  ctx.lineWidth = size * 2
+  ctx.stroke()
+}
+
+function updateSizeOnScreen() {
+  sizeEL.innerText = size
+}
+
+increaseBtn.addEventListener('click', () => {
+  size += 5
+
+  if (size > 50) {
+    size = 50
+  }
+
+  updateSizeOnScreen()
+})
+
+decreaseBtn.addEventListener('click', () => {
+  size -= 5
+
+  if (size < 5) {
+    size = 5
+  }
+
+  updateSizeOnScreen()
+})
+
+colorEl.addEventListener('change', (e) => (color = e.target.value))
+
+clearEl.addEventListener('click', () =>
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+)
